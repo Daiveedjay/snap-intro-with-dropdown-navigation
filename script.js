c0c85f1f2e05d959c4791729297924f9934fe3e1
Daiveed <jajadavidjid@gmail.com>
2022-07-29 21:16:45 +0100

    Landing Page not fully responsive. Hamburger Menu and Dropdown Menu fully responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileIcon = document.querySelector(".nav-open");
+
+mobileIcon.addEventListener("click", function () {
+  mobileNav.classList.toggle("active");
+
+  if (mobileNav.classList.contains("active")) {
+    mobileIcon.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  } else {
+    mobileIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  }
+});
